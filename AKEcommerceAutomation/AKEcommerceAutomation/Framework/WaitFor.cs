be3971c69a7be8d819feee18fd73830116dc63ab
teja-vellanki <tvellanki@gmail.com>
2014-02-05 08:13:15 +0000

    Installed Packages of Selenium and SpecFlow

--- AKEcommerceAutomation/AKEcommerceAutomation/Framework/WaitFor.cs
+++ AKEcommerceAutomation/AKEcommerceAutomation/Framework/WaitFor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using gherkin.lexer.i18n;
+using gherkin.lexer;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
