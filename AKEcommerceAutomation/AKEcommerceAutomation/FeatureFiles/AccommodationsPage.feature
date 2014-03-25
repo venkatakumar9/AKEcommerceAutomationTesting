b11f29fe0793a0b29502d93a87fdd97739515e08
teja-vellanki <tvellanki@gmail.com>
2014-03-25 17:20:32 +0000

    Added Accomodation Scenario

--- AKEcommerceAutomation/AKEcommerceAutomation/FeatureFiles/AccommodationsPage.feature
+++ AKEcommerceAutomation/AKEcommerceAutomation/FeatureFiles/AccommodationsPage.feature
@@ -4,8 +4,12 @@
 	I want to be told the sum of two numbers
 
 @mytag
-Scenario: Add two numbers
-	Given I have entered 50 into the calculator
-	And I have entered 70 into the calculator
-	When I press add
-	Then the result should be 120 on the screen
+Scenario: Reaching the Sanctuary Chobe Chilwero Accomodation Page
+Given I am on the HomePage
+And I reach Africa Continent page
+And I reach the Botswana Page
+And I reach the Chobe Area page
+When I click on Accommodations tab
+Then All the Avialable Accomodatioons are present
+When I click on Sanctuary Chobe Chilwero
+Then I reach Sanctuary Chobe Chilwero Accomodation page
---
+++ AKEcommerceAutomation/AKEcommerceAutomation/TestSteps/AccommodationsPageSteps.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using AKEcommerceAutomation.Framework;
+using AKEcommerceAutomation.PageObjects;
+using AKEcommerceAutomation.PageObjects.Object_Repository;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace AKEcommerceAutomation
+{
+    [Binding]
+    public class AccommodationsPageSteps : SeleniumTestBase
+    {
+        [Given(@"I reach the Chobe Area page")]
+        public void GivenIReachTheChobeAreaPage()
+        {
+            new BotswanaAreaTab().WhenIClickOnAREAS();
+            new ChobeAreaPageSteps().WhenIClickOnChobe();
+        }
+        
+        [When(@"I click on Accommodations tab")]
+        public void WhenIClickOnAccommodationsTab()
+        {
+            string[] navlinks = new ContinentPage(driver).continentnavlinks();
+            foreach (var navlink in navlinks)
+            {
+                if (navlink=="ACCOMMODATIONS")
+                {
+                    driver.FindElement(By.LinkText(navlink)).Click();
+                }
+                
+            }
+        }
+        
+        [When(@"I click on Sanctuary Chobe Chilwero")]
+        public void WhenIClickOnSanctuaryChobeChilwero()
+        {
+           foreach (IWebElement accomodation in driver.FindElements(ContinentPageElements.countries))
+                {
+                    if (accomodation.Text == "SANCTUARY CHOBE CHILWERO-CHOBE")
+                    {
+                        accomodation.Click();
+                        break;
+                    }
+                }
+        }
+
+        [Then(@"All the Avialable Accomodatioons are present")]
+        public void ThenAllTheAvialableAccomodatioonsArePresent()
+        {
+            string[] accomodations= new ContinentPage(driver).countries();
+            foreach (var accomodation in accomodations)
+            {
+                Console.WriteLine(accomodation);
+            }
+        }
+
+        [Then(@"I reach Sanctuary Chobe Chilwero Accomodation page")]
+        public void ThenIReachSanctuaryChobeChilweroAccomodationPage()
+        {
+            Assert.AreEqual("Sanctuary Chobe Chilwero Lodge, Chobe", driver.Title);
+        }
+    }
+}
